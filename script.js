625e96715c861ae2c3248a7c07b04118f3322e28
alduanemirasol <alduanecuevasmirasol@gmail.com>
2025-12-13 00:17:23 +0800

    updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apkURL = "https://www.dropbox.com/scl/fi/s8ecfjn2aa1pjkce0gd3g/E.M.P.O.W.E.R.apk?rlkey=65a40qlagvdnzxeyf78qltxbg&st=hnkcrwge&dl=1";
+const apkURL = "https://www.dropbox.com/scl/fi/s8ecfjn2aa1pjkce0gd3g/E.M.P.O.W.E.R.apk?rlkey=65a40qlagvdnzxeyf78qltxbg&st=wowu5t3q&dl=1";
 const AUTO_DOWNLOAD = 1;
 
 const downloadBtn = document.getElementById("downloadBtn");
